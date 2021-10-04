13d1bbe2f86831ff6ada3fd14f7fe1bc8fb8de82
Tuhgtuhgtuhg <tuhgtuhgtuhg@gmail.com>
2021-10-04 23:29:36 +0300

    Added basic version of ping pong game

---
+++ PIngPong/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace PIngPong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.background = new System.Windows.Forms.Panel();
+            this.loseLabel = new System.Windows.Forms.Label();
+            this.result = new System.Windows.Forms.Label();
+            this.gameBall = new System.Windows.Forms.PictureBox();
+            this.gamePanel = new System.Windows.Forms.PictureBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.gameBall1 = new System.Windows.Forms.PictureBox();
+            this.background.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gameBall)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gamePanel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gameBall1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // background
+            // 
+            this.background.Controls.Add(this.gameBall1);
+            this.background.Controls.Add(this.loseLabel);
+            this.background.Controls.Add(this.result);
+            this.background.Controls.Add(this.gameBall);
+            this.background.Controls.Add(this.gamePanel);
+            this.background.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.background.Location = new System.Drawing.Point(0, 0);
+            this.background.Margin = new System.Windows.Forms.Padding(4);
+            this.background.Name = "background";
+            this.background.Size = new System.Drawing.Size(1067, 554);
+            this.background.TabIndex = 0;
+            // 
+            // loseLabel
+            // 
+            this.loseLabel.AutoSize = true;
+            this.loseLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 30.45F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.loseLabel.Location = new System.Drawing.Point(347, 74);
+            this.loseLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.loseLabel.Name = "loseLabel";
+            this.loseLabel.Size = new System.Drawing.Size(0, 59);
+            this.loseLabel.TabIndex = 3;
+            // 
+            // result
+            // 
+            this.result.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.result.AutoSize = true;
+            this.result.Font = new System.Drawing.Font("Arial Black", 17.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.result.Location = new System.Drawing.Point(25, 52);
+            this.result.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(148, 41);
+            this.result.TabIndex = 2;
+            this.result.Text = "Score: 0";
+            // 
+            // gameBall
+            // 
+            this.gameBall.BackColor = System.Drawing.Color.Blue;
+            this.gameBall.Location = new System.Drawing.Point(13, 4);
+            this.gameBall.Margin = new System.Windows.Forms.Padding(4);
+            this.gameBall.Name = "gameBall";
+            this.gameBall.Size = new System.Drawing.Size(67, 62);
+            this.gameBall.TabIndex = 1;
+            this.gameBall.TabStop = false;
+            // 
+            // gamePanel
+            // 
+            this.gamePanel.BackColor = System.Drawing.Color.Red;
+            this.gamePanel.Location = new System.Drawing.Point(357, 479);
+            this.gamePanel.Margin = new System.Windows.Forms.Padding(4);
+            this.gamePanel.Name = "gamePanel";
+            this.gamePanel.Size = new System.Drawing.Size(227, 32);
+            this.gamePanel.TabIndex = 0;
+            this.gamePanel.TabStop = false;
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // gameBall1
+            // 
+            this.gameBall1.BackColor = System.Drawing.Color.Blue;
+            this.gameBall1.Location = new System.Drawing.Point(1000, 4);
+            this.gameBall1.Margin = new System.Windows.Forms.Padding(4);
+            this.gameBall1.Name = "gameBall1";
+            this.gameBall1.Size = new System.Drawing.Size(67, 62);
+            this.gameBall1.TabIndex = 4;
+            this.gameBall1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.background);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.background.ResumeLayout(false);
+            this.background.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gameBall)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gamePanel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gameBall1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel background;
+        private System.Windows.Forms.PictureBox gameBall;
+        private System.Windows.Forms.PictureBox gamePanel;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label result;
+        private System.Windows.Forms.Label loseLabel;
+        private System.Windows.Forms.PictureBox gameBall1;
+    }
+}
+
---
+++ PIngPong/Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Media;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PIngPong
+{
+    public partial class Form1 : Form
+    {
+
+        private int speed_vertical = 2;
+        private int speed_hor = 2;
+        private int speed_vertical1 = 2;
+        private int speed_hor1 = 2;
+        private int score = 0;
+        private int melody = 0;
+        private SoundPlayer player = new SoundPlayer();
+        private string path = AppDomain.CurrentDomain.BaseDirectory;
+        private Random randColor = new Random();
+        private Random randX = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer.Enabled = true;
+
+            Cursor.Hide();
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.TopMost = true;
+
+            this.Bounds = Screen.PrimaryScreen.Bounds;
+
+            gamePanel.Top = background.Bottom - (background.Bottom / 10);
+            result.Left  = (background.Width / 2) - (result.Width / 2);
+            loseLabel.Visible = false;
+            
+            loseLabel.Top = (background.Height / 2) - (loseLabel.Height / 2);
+            loseLabel.Text = "Ви програли! \nДля наступної спроби натисніть \"R\" \nДля виходу - \"Esc\"";
+            loseLabel.Left = (background.Width / 2) - (loseLabel.Width / 2);
+            player.SoundLocation = path + "\\music\\1.wav";
+            player.Play();
+
+            System.Drawing.Drawing2D.GraphicsPath tmp = new System.Drawing.Drawing2D.GraphicsPath();
+            tmp.AddEllipse(0, 0, gameBall.Width, gameBall.Height);
+            Region rgn = new Region(tmp);
+            gameBall.Region = rgn;
+            gameBall1.Region = rgn;
+            do
+            {
+                
+                gameBall.BackColor = Color.FromArgb(randColor.Next(150, 255), randColor.Next(150, 255), randColor.Next(150, 255));
+                gameBall1.BackColor = Color.FromArgb(randColor.Next(150, 255), randColor.Next(150, 255), randColor.Next(150, 255));
+            } while (background.BackColor == gameBall1.BackColor || background.BackColor == gameBall.BackColor);
+
+            do
+            {
+                gameBall.Top = randX.Next(0, this.Height / 3);
+                gameBall.Left = randX.Next(0, this.Width - gameBall.Width);
+                speed_hor = 2;
+                speed_vertical = 2;
+                score = 0;
+                gameBall1.Top = randX.Next(1, this.Height / 3);
+                gameBall1.Left = randX.Next(1, this.Width - gameBall1.Width);
+            } while (gameBall.Bounds.IntersectsWith(gameBall1.Bounds));
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+                this.Close();
+
+            if(e.KeyCode == Keys.R)
+            {
+                do
+                {
+                    gameBall.Top = randX.Next(0, this.Height / 3);
+                    gameBall.Left = randX.Next(0, this.Width - gameBall.Width);
+                    speed_hor = 2;
+                    speed_vertical = 2;
+                    score = 0;
+                    gameBall1.Top = randX.Next(1, this.Height / 3);
+                    gameBall1.Left = randX.Next(1, this.Width - gameBall1.Width);
+                } while (gameBall.Bounds.IntersectsWith(gameBall1.Bounds));
+                
+
+
+                speed_hor1 = 2;
+                speed_vertical1 = 2;
+                
+                loseLabel.Visible = false;
+                timer.Enabled = true;
+                result.Text = "Score: 0";
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            gamePanel.Left = Cursor.Position.X - (gamePanel.Width / 2);
+
+            gameBall.Left += speed_hor;
+            gameBall.Top += speed_vertical;
+            gameBall1.Left += speed_hor1;
+            gameBall1.Top += speed_vertical1;
+
+
+            if (gameBall.Left <= background.Left)
+                speed_hor *= -1;
+
+            if (gameBall.Right >= background.Right)
+                speed_hor *= -1;
+
+            if (gameBall.Top <= background.Top)
+                speed_vertical *= -1;
+
+            if (gameBall.Bottom >= background.Bottom || gameBall1.Bottom >= background.Bottom)
+            {
+                loseLabel.Visible = true;
+                timer.Enabled = false;
+            }
+            if (gameBall.Bounds.IntersectsWith(gameBall1.Bounds))
+            {
+                speed_hor *= -1;
+                speed_hor1 *= -1;
+            }
+            if(gameBall.Bounds.IntersectsWith(gamePanel.Bounds))
+            {
+                speed_hor += 2;
+                speed_vertical += 2;
+                speed_vertical *= -1;
+                score += 1;
+                result.Text = "Score: " + score.ToString();
+
+                
+                do
+                {
+
+                    gameBall.BackColor = Color.FromArgb(randColor.Next(150, 255), randColor.Next(150, 255), randColor.Next(150, 255));
+                } while (background.BackColor == gameBall.BackColor);
+
+                if (score % 10 == 0)
+                {
+                    do
+                    {
+                        background.BackColor = Color.FromArgb(randColor.Next(150, 255), randColor.Next(150, 255), randColor.Next(150, 255));
+                    } while (background.BackColor == gameBall1.BackColor || background.BackColor == gameBall.BackColor);
+
+                    player.Stop();
+                    melody++;
+
+                    switch (melody % 3)
+                    {
+                        case 1:
+                            player.SoundLocation = path + "\\music\\2.wav";
+                            break;
+                        case 2:
+                            player.SoundLocation = path + "\\music\\3.wav";
+                            break;
+                        default:
+                            melody = 0;
+                            player.SoundLocation = path + "\\music\\1.wav";
+                            break;
+                    }
+                    player.Play();
+                }
+
+            }
+
+            if (gameBall1.Left <= background.Left)
+                speed_hor1 *= -1;
+
+            if (gameBall1.Right >= background.Right)
+                speed_hor1 *= -1;
+
+            if (gameBall1.Top <= background.Top)
+                speed_vertical1 *= -1;
+
+            if (gameBall1.Bounds.IntersectsWith(gamePanel.Bounds))
+            {
+                speed_hor1 += 2;
+                speed_vertical1 += 2;
+                speed_vertical1 *= -1;
+                score += 1;
+                result.Text = "Score: " + score.ToString();
+
+
+                do
+                {
+
+                    gameBall1.BackColor = Color.FromArgb(randColor.Next(150, 255), randColor.Next(150, 255), randColor.Next(150, 255));
+                } while (background.BackColor == gameBall1.BackColor);
+
+                if (score % 10 == 0)
+                {
+                    do
+                    {
+                        background.BackColor = Color.FromArgb(randColor.Next(150, 255), randColor.Next(150, 255), randColor.Next(150, 255));
+                    } while (background.BackColor == gameBall1.BackColor || background.BackColor == gameBall.BackColor);
+
+                    player.Stop();
+                    melody++;
+
+                    switch (melody % 3)
+                    {
+                        case 1:
+                            player.SoundLocation = path + "\\music\\2.wav";
+                            break;
+                        case 2:
+                            player.SoundLocation = path + "\\music\\3.wav";
+                            break;
+                        default:
+                            melody = 0;
+                            player.SoundLocation = path + "\\music\\1.wav";
+                            break;
+                    }
+                    player.Play();
+                }
+
+            }
+
+
+        }
+  
+    }
+}
